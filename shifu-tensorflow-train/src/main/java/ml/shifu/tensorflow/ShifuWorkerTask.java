e737066dcc90b597dc286a77d886e77db6a3bdfd
MiniZhuwei <minizhuwei@gmail.com>
2018-12-05 16:54:47 +0800

    remove train, we put the train in shifu

--- shifu-tensorflow-train/src/main/java/ml/shifu/tensorflow/ShifuWorkerTask.java
+++
@@ -1,9 +0,0 @@
-package ml.shifu.tensorflow;
-
-public class ShifuWorkerTask {
-    
-    public static void main(String[] args) throws Throwable {
-        System.out.println("Hello world");
-        Thread.sleep(10000);
-    }
-}
