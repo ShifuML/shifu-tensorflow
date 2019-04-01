6abea0bbb379448cb11590f095336ef29331c2d7
pengshan.zhang <pengshan.zhang@gmail.com>
2019-04-01 17:45:49 +0800

    fix ut issue of shifu-tf-eval

--- shifu-tensorflow-eval/src/main/java/ml/shifu/shifu/tensorflow/TensorflowModel.java
+++ shifu-tensorflow-eval/src/main/java/ml/shifu/shifu/tensorflow/TensorflowModel.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright [2013-2018] PayPal Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ml.shifu.shifu.tensorflow;
 
 import org.encog.ml.data.MLData;
 import ml.shifu.shifu.core.Computable;
+import scala.collection.mutable.ArrayBuilder.ofBoolean;
 import ml.shifu.shifu.container.obj.GenericModelConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,15 +58,15 @@ public class TensorflowModel implements Computable {
             double[] inputDArr = input.getData();
             float[] inputFArr = new float[inputDArr.length];
             for(int i = 0; i < inputDArr.length; i++) {
-                inputFArr[i] = (float)inputDArr[i];
+                inputFArr[i] = (float) inputDArr[i];
             }
-            
-            runner.feed(inputNames[0], Tensor.create(new float[][]{inputFArr}));
+
+            runner.feed(inputNames[0], Tensor.create(new float[][] { inputFArr }));
 
             for(int i = 1; i < inputNames.length; i++) {
                 try {
                     runner.feed(inputNames[i], Tensor.create(properties.get(inputNames[i])));
-                } catch(Exception e) {
+                } catch (Exception e) {
                     LOG.error("Invalid input, {}", e);
                 }
             }
@@ -66,7 +82,7 @@ public class TensorflowModel implements Computable {
 
     @Override
     public void init(GenericModelConfig config) {
-        LOG.error("init tensorflow model");
+        LOG.info("Init tensorflow model");
         if(this.initiate) {
             return;
         }
@@ -75,24 +91,37 @@ public class TensorflowModel implements Computable {
             throw new RuntimeException("Config is null");
         }
 
-        this.config = config; 
+        this.config = config;
         properties = this.config.getProperties();
         if(properties == null || properties.size() == 0) {
             LOG.error("Properties is null");
             throw new RuntimeException("Properties is null");
         }
-        this.modelPath = (String)properties.get("modelpath");
-        this.inputNames = config.getInputnames().toArray(new String[config.getInputnames().size()]);
-        this.outputNames = (String)properties.get("outputnames");
-        this.tags = ((List<String>)properties.get("tags")).toArray(new String[((List<String>)properties.get("tags")).size()]);
+        this.modelPath = (String) properties.get("modelpath");
+        this.inputNames = config.getInputnames().toArray(new String[0]);
+        Object outputNames = properties.get("outputnames");
+        if(outputNames instanceof String) {
+            this.outputNames = (String) properties.get("outputnames");
+        }  else if(outputNames instanceof String[]) {
+            String[] outputs = (String[])outputNames;
+            if(outputs.length == 1) {
+                this.outputNames = outputs[0];
+            } else {
+                throw new IllegalArgumentException("Output now only support single output in inference.");
+            }
+        }
 
-        LOG.error("Properties : {}", properties);
+        @SuppressWarnings("unchecked")
+        List<String> tagList = (List<String>) properties.get("tags");
+        this.tags = tagList.toArray(new String[tagList.size()]);
+
+        LOG.info("Debug: properties : {}", properties);
 
         if(this.modelPath == null || this.modelPath.isEmpty()) {
             LOG.error("Model path is null");
             throw new RuntimeException("Model path is null");
         }
-        
+
         if(this.inputNames == null || this.inputNames.length == 0) {
             LOG.error("Input names is null");
             throw new RuntimeException("Input names is null");
@@ -108,14 +137,13 @@ public class TensorflowModel implements Computable {
             throw new RuntimeException("Tags is null");
         }
 
-        LOG.error("Load model from " + this.modelPath);
-        this.smb = SavedModelBundle.load(modelPath, tags);
-        LOG.error("init tensorflow model done");
+        LOG.info("Load model from {}.", this.modelPath);
+        this.smb = SavedModelBundle.load(modelPath, this.tags);
+        LOG.info("Init tensorflow model done.");
         initiate = true;
     }
 
     @Override
     public void releaseResource() {
-
     }
 }
--- shifu-tensorflow-eval/src/test/java/ml/shifu/shifu/tensorflow/TensorflowModelTest.java
+++ shifu-tensorflow-eval/src/test/java/ml/shifu/shifu/tensorflow/TensorflowModelTest.java
@@ -1,27 +1,50 @@
+/*
+ * Copyright [2013-2018] PayPal Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ml.shifu.shifu.tensorflow;
 
-import org.junit.Test;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 import org.encog.ml.data.MLData;
 import org.encog.ml.data.basic.BasicMLData;
-import ml.shifu.shifu.container.obj.GenericModelConfig;
+import org.junit.Test;
 
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Random;
+import ml.shifu.shifu.container.obj.GenericModelConfig;
 
 public class TensorflowModelTest {
 
     @Test
     public void testCompute() {
-        Map<String, Object> properties = new HashMap<String, Object>();         
-        properties.put("tags", new String[]{"serve"});
-        properties.put("inputnames", new String[]{"dense_46_input", "dropout_1/keras_learning_phase"});
-        properties.put("outputnames", new String[]{"dense_66/Sigmoid"});
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put("tags", Arrays.asList("serve"));
+        properties.put("inputnames", new String[] { "dense_46_input", "dropout_1/keras_learning_phase" });
+        properties.put("outputnames", new String[] { "dense_66/Sigmoid" });
         properties.put("dropout_1/keras_learning_phase", false);
-        properties.put("modelpath",
-            "/Users/wzhu1/workspace/shifu-tensorflow/shifu-tensorflow-eval/src/test/resource/dummydl");
+        properties.put("modelpath", System.getProperty("user.dir") + File.separator + "src/test/resources/dummydl");
 
         GenericModelConfig config = new GenericModelConfig();
+        List<String> inputs = new ArrayList<String>();
+        inputs.add("dense_46_input");
+        inputs.add("dropout_1/keras_learning_phase");
+        config.setInputnames(inputs);
         config.setProperties(properties);
         double[] inputArr = new double[1522];
         Random random = new Random();
@@ -33,6 +56,5 @@ public class TensorflowModelTest {
         tm.init(config);
         double result = tm.compute(input);
         System.out.println(result);
-         
     }
 }
